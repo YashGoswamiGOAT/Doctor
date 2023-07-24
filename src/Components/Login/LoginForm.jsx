ca4097b84579d2971f351f27097750dc5c295f67
yashg <yashgoswamigoat@gmail.com>
2023-07-24 15:58:56 +0530

    creating login form

---
+++ src/Components/Login/LoginForm.jsx
@@ -0,0 +1,11 @@
+export default function LoginForm()
+{
+    return (
+        <>
+            <div className={'loginForm'}>
+                <input type={'number'} id={'phone'}/>
+                <input type={'password'} id={'password'}/>
+            </div>
+        </>
+    )
+}
--- src/Components/Login/login.jsx
+++ src/Components/Login/login.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import {useContext} from "react";
 import Account from "../../Contexts/account";
+import './login.css' ;
+import banner from '../../images/design1.png' ;
+import logo from '../../images/Logo.png' ;
+
 export default function Login(){
     let account = useContext(Account) ;
     return (
         <>
-            <div className={''}></div>
+            <div className={'homeScreenGrid'}>
+                <img src={banner} className={'banner'}/>
+                <div className={'loginForm'}>
+                    <input type={'number'} id={'phone'}/>
+                    <input type={'password'} id={'password'}/>
+                </div>
+                <div className={'footer'}>
+                    <img src={logo} className={'logo'}/>
+                </div>
+            </div>
         </>
     )
 }
